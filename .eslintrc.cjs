730ef661199d98c14808f59c0bed9ab1b91dbec4
Kim Dekker <kim11dekker@gmail.com>
2025-09-21 17:28:40 +0200

    Redo eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,27 @@
+// eslintrc.cjs
 module.exports = {
   root: true,
-  env: { node: true, browser: true },
-  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
-  plugins: ["vue", "prettier"],
-  parserOptions: { ecmaVersion: 2020, sourceType: "module" },
+  env: { node: true, browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // TS rules
+    'plugin:vue/vue3-recommended', // Vue 3 rules
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier + displays Prettier errors as ESLint
+  ],
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser', // Use TS parser inside Vue files
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
-    "prettier/prettier": "error",
+    // Prettier integration
+    'prettier/prettier': 'error',
+
+    // Example stricter rules (tweak to taste)
+    'vue/multi-word-component-names': 'off', // Allow single-word component names
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
