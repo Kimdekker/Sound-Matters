03ed28e06806783fb8b25d6306c3c28fd2049377
Kim Dekker <kim11dekker@gmail.com>
2025-10-12 22:31:32 +0200

    Making a homepage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <script setup lang="ts">
-  import Button from './components/Button.vue'
-  import Typography from './components/Typography.vue'
+  import index from '../src/pages/index.vue'
 
 </script>
 
 <template>
-  <main>
+  <index />
 
-    <Button label="Button" primary size="medium" />
-    <Button label="Secondary button" size="medium" />
-
-    <Typography as="h1">Heading 1</Typography>
-    <Typography as="h2">Heading 2</Typography>
-    <Typography as="h3">Heading 3</Typography>
-    <Typography as="h4">Heading 4</Typography>
-    <Typography as="h5">Heading 5</Typography>
-    <Typography as="h6">Heading 6</Typography>
-    <Typography as="p">Paragraph</Typography>
-    <Typography as="span">Span</Typography>
-
-  </main>
-
-  <footer class="p-4 text-sm text-gray-500">
+  <footer class="p-4 text-sm text-[var(--color-grey)] text-center">
     © {{ new Date().getFullYear() }} Sound Matters by The Fusion Initiative
   </footer>
 </template>
---
+++ src/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import Typography from '../components/Typography.vue'
+
+</script>
+
+<template>
+  <main class="flex flex-col justify-center items-center h-screen">
+
+    <Typography as="h1" class="text-center">
+        <div class="inline">S</div>
+        <div class="inline">o</div>
+        <div class="inline">u</div>
+        <div class="inline">n</div>
+        <div class="inline">d</div>
+        <div class="inline">M</div>
+        <div class="inline">a</div>
+        <div class="inline">t</div>
+        <div class="inline">t</div>
+        <div class="inline">e</div>
+        <div class="inline">r</div>
+        <div class="inline">s</div>
+    </Typography>
+
+    <Typography as="h3" class="text-center w-lg">
+        On a Journey to Protect Our Ears and Celebrate the Joy of Sound
+    </Typography>
+
+    <Typography as="span" class="text-center">
+        the Fusion Initiative
+    </Typography>
+
+  </main>
+
+</template>
